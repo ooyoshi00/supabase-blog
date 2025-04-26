52866ba05adbbc53bba8029266e337863e759c7a
yoshioka <yosshioka0022@gmail.com>
2025-04-26 15:45:40 +0900

    ログイン機能を追加

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -29,21 +29,19 @@ export async function updateSession(request: NextRequest) {
     }
   )
 
-  // 必要に応じてリダイレクトなどの処理を行う
-  // const {
-  //   data: { user },
-  // } = await supabase.auth.getUser()
-
-  // if (
-  //   !user &&
-  //   !request.nextUrl.pathname.startsWith("/login") &&
-  //   !request.nextUrl.pathname.startsWith("/signup")
-  // ) {
-  //   const url = request.nextUrl.clone()
-  //   url.pathname = "/login"
-
-  //   return NextResponse.redirect(url)
-  // }
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+  if (
+    !user &&
+    !request.nextUrl.pathname.startsWith('/login') &&
+    !request.nextUrl.pathname.startsWith('/auth')
+  ) {
+    // no user, potentially respond by redirecting the user to the login page
+    const url = request.nextUrl.clone()
+    url.pathname = '/login'
+    return NextResponse.redirect(url)
+  }
 
   return supabaseResponse
 }
