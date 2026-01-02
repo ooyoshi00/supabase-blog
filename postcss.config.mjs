29816a6b3f902f90425a1aca0c3d5886714664b6
yoshioka <yosshioka0022@gmail.com>
2026-01-02 10:48:44 +0900

    postcssの設定を修正

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,5 @@
-import tailwindcss from "@tailwindcss/postcss";
-import autoprefixer from "autoprefixer";
-
 const config = {
-  plugins: [tailwindcss(), autoprefixer()],
+  plugins: ["@tailwindcss/postcss", "autoprefixer"],
 };
 
 export default config;
