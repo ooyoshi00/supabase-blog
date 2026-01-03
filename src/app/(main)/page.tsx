0db8caf2d179179ffd7c57e1efbe2c278744948b
yoshioka <yosshioka0022@gmail.com>
2026-01-03 20:07:22 +0900

    経歴セクションの職歴を更新し、役職名を詳細に修正

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -9,8 +9,8 @@ const Home = () => {
       <section className="bg-white p-6 rounded-lg shadow-md">
         <h2 className="text-2xl font-bold mb-4 text-blue-800">経歴</h2>
         <ul className="list-disc list-inside space-y-2 text-gray-700">
-          <li>2021年 - 2024年: 不動産テック系会社 - Webエンジニア</li>
-          <li>2024年 - 現在: スペースシェア系会社 - Webエンジニア</li>
+          <li>2021年 - 2024年: 不動産テック関連会社 - フロントエンドエンジニア, スクラムマスター</li>
+          <li>2024年 - 現在: レンタルスペース関連会社 - Webエンジニア</li>
         </ul>
       </section>
       <Suspense fallback={<div>Loading...</div>}>
